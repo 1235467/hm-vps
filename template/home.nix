267fb3156c29d402a350efe29f18dbe8142e8774
xuxin <qq20099@outlook.com>
2023-11-14 23:20:17 +0800

    init

---
+++ template/home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+let
+ variables = import ./variables.nix;
+in
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "${variables.username}";
+  home.homeDirectory = "/home/${variables.username}";
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.packages = [
+    pkgs.arion
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+  };
+
+  programs.home-manager.enable = true;
+}
